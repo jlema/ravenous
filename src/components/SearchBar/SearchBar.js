6507cf13e1dd05223a37467f6f1c999d6ae81b32
John (Juan) Lema <jlema@users.noreply.github.com>
2022-03-13 17:41:06 -0700

    add visual feedback for sorting options menu add stub for Yelp API search functionality

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,18 +1,67 @@
 import React from 'react'
 import './SearchBar.css'
 
-const sortByOptions = {
-	best_match: 'Best Match',
-	rating: 'Highest Rated',
-	review_count: 'Most Reviewed',
-}
-
 class SearchBar extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			term: '',
+			location: '',
+			sortBy: 'best_match',
+		}
+		this.sortByOptions = {
+			best_match: 'Best Match',
+			rating: 'Highest Rated',
+			review_count: 'Most Reviewed',
+		}
+		this.handleTermChange = this.handleTermChange.bind(this)
+		this.handleLocationChange = this.handleLocationChange.bind(this)
+		this.handleSearch = this.handleSearch.bind(this)
+	}
+
+	getSortByClass(sortByOption) {
+		if (this.state.sortBy === sortByOption) {
+			return 'active'
+		} else {
+			return ''
+		}
+	}
+
+	handleSortByChange(sortByOption) {
+		this.setState({ sortBy: sortByOption })
+	}
+
+	handleTermChange(event) {
+		this.setState({ term: event.target.value })
+	}
+
+	handleLocationChange(event) {
+		this.setState({ location: event.target.value })
+	}
+
+	handleSearch(event) {
+		this.props.searchYelp(
+			this.state.term,
+			this.state.location,
+			this.state.sortBy
+		)
+		event.preventDefault()
+	}
+
 	renderSortByOptions() {
-		return Object.keys(sortByOptions).map(sortByOptionKey => {
-			return <li key={sortByOptionKey}>{sortByOptions[sortByOptionKey]}</li>
+		return Object.keys(this.sortByOptions).map(sortByOptionKey => {
+			const sortByOptionValue = this.sortByOptions[sortByOptionKey]
+			return (
+				<li
+					key={sortByOptionKey}
+					className={this.getSortByClass(sortByOptionKey)}
+					onClick={this.handleSortByChange.bind(this, sortByOptionKey)}>
+					{sortByOptionValue}
+				</li>
+			)
 		})
 	}
+
 	render() {
 		return (
 			<div className='SearchBar'>
@@ -20,11 +69,14 @@ class SearchBar extends React.Component {
 					<ul>{this.renderSortByOptions()}</ul>
 				</div>
 				<div className='SearchBar-fields'>
-					<input placeholder='Search Businesses' />
-					<input placeholder='Where?' />
+					<input
+						placeholder='Search Businesses'
+						onChange={this.handleTermChange}
+					/>
+					<input placeholder='Where?' onChange={this.handleLocationChange} />
 				</div>
 				<div className='SearchBar-submit'>
-					<a>Let's Go</a>
+					<a onClick={this.handleSearch}>Let's Go</a>
 				</div>
 			</div>
 		)
